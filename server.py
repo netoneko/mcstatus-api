34c84e74b2ac0beb006b643f7088934c88643659
uniquelogin <pavel.zuev@gmail.com>
2014-09-17 03:37:37 +0600

    fixes, cleanups

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask, request, jsonify
 from minecraft_query import MinecraftQuery
 from urlparse import urlparse
@@ -6,19 +8,22 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-r", "--redis-url", dest="redis_url", help="Redis URL", metavar="REDIS_URL")
-parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose mode")
-parser.add_option("-s", "--simple", action="store_true", dest="simple", default=False, help="Disable cache")
+parser.add_option("-s", "--simple", action="store_true", dest="simple_mode", default=False, help="Disable Redis")
+parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False, help="Enable Flask debug mode")
+parser.add_option("-p", "--port", action="store", type="int", dest="port", default=34500, help="HTTP server listen port")
 
 (options, args) = parser.parse_args()
 
 def init_redis():
-  from redis import StrictRedis
+  import redis
   url = urlparse(options.redis_url or 'redis://0@localhost:6379')
-  return StrictRedis(host=url.hostname, port=url.port, db=url.username)
+  return redis.StrictRedis(host=url.hostname, port=url.port, db=url.username)
 
 app = Flask(__name__)
-if not options.simple:
+if not options.simple_mode:
   redis = init_redis()
+else:
+  redis = None
 
 @app.route('/')
 def index():
@@ -27,11 +32,18 @@ def index():
 @app.route('/api/status', methods=['GET'])
 def status():
   host = request.args.get('host')
-  port = request.args.get('port', default=25565)
-  test = bool(request.args.get('test', default=False))
+  try:
+    port = int(request.args.get('port', default=25565))
+  except Exception, e:
+    port = 25565
+
+  test = (host == 'test.mc.glassmoon.ru') 
+  if test:
+    return jsonify(players=['doge', 'such', 'wow', 'diamonds']) 
+  
   result = None
 
-  if not options.simple:
+  if redis:
     key = "%s:%s" % (host, str(port))
     result = redis.hgetall(key)
 
@@ -39,16 +51,14 @@ def status():
     try:
       result = MinecraftQuery(host, port).get_rules()
     except Exception as e:
-      result = {'message': 'Socket error, host is unreacheable'}
+      result = {'message': 'No response from the Minecraft server'}
 
-    if not options.simple:
+    if redis:
       redis.hmset(key, result)
       redis.pexpire(key, 30000) # 30s
-
-  if test:
-    result['players'] = ['Wow', 'Such', 'Doge']
 
   return jsonify(**result)
 
 if __name__ == '__main__':
-  app.run(debug=options.verbose)
+  app.run(debug=options.debug, port=options.port)
+
